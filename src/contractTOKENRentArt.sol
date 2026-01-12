14fa2e64e00290e31357cfc875a57bb71e8f6a8a
ismail.abloua <ismail.edu@proton.me>
2026-01-12 16:04:55 +0100

    token  rentart

---
+++ src/contractTOKENRentArt.sol
@@ -0,0 +1,15 @@
+// contractTOKENRentArt.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RentArtToken is ERC20 {
+    constructor() ERC20("RentArt Token", "RENTART") {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1 million de tokens pour le testeur
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
