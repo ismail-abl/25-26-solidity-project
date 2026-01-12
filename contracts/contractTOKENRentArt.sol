586ee50f2e758fff3dbea52d84ea09513cffe495
ismail.abloua <ismail.abloua@etu.hes-ge.ch>
2026-01-12 19:50:03 +0100

    déplacé les fichiers, rapport est README.md

--- contracts/contractTOKENRentArt.sol
+++ contracts/contractTOKENRentArt.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.5.0/token/ERC20/ERC20.sol";
 
 contract RentArtToken is ERC20 {
     constructor() ERC20("RentArt Token", "RENTART") {
